656478c1340bedcd3ae7b7737221c29d77bd34db
douglasbrandao21 <douglasbrando5@gmail.com>
2020-05-08 18:34:29 -0300

    Challenged finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -9,20 +9,44 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public setBalance(balance: Balance): void {
+    this.balance = balance;
+  }
+
+  public create(transaction: Transaction): Transaction {
+    this.transactions.push(transaction);
+
+    if (transaction.type === 'income') {
+      this.balance.income += transaction.value;
+    } else {
+      if (transaction.value > this.balance.total)
+        throw new Error('Your money is not enougth');
+
+      this.balance.outcome += transaction.value;
+    }
+
+    this.balance.total = this.balance.income - this.balance.outcome;
+
+    return transaction;
   }
 }
 
